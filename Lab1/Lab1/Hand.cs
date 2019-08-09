9daee890879788059f5488d84b9db834662aa78b
rcorrea4 <rcorrea4@miuandes.cl>
2019-08-09 10:27:05 -0400

    lab1

---
+++ Lab1/Lab1/Hand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Hand
+    {
+        public List<string> combatcard;
+        public List<string> specialcard;
+
+        public Hand(List<string> combatcard2, List<string> specialcard2)
+        {
+            combatcard = combatcard2;
+            specialcard = specialcard2;
+        }
+        public List<string> Combatcard
+        {
+            get
+            {
+                return combatcard;
+
+            }
+            set
+            {
+                combatcard = value;
+            }
+        }
+    }
+}
